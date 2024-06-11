533d3a9149751736bc3cea1b11de8ab99aabbfe8
maxpp2910 <107290604+maxpp2910@users.noreply.github.com>
2024-06-11 15:54:33 -0500

    Update loginView.dart

--- loggy_prototype/lib/loginView.dart
+++ loggy_prototype/lib/loginView.dart
@@ -5,6 +5,71 @@ class loginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50], // Fondo azul claro
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // Logo
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  child: Image.asset( //FALTA AGREGAR LA CARPETA ASSETS PARA AGREGAR EL LOGO
+                    'assets/logo.png', // Asegúrate de tener el logo en tu carpeta assets
+                    height: 150,
+                  ),
+                ),
+                // Título de la app
+                const Text(
+                  'Loggy',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Campo de texto para el nombre de usuario
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nombre de usuario',
+                    hintText: 'Ingrese su nombre de usuario',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Campo de texto para la contraseña
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Contraseña',
+                    hintText: 'Ingrese su contraseña',
+                  ),
+                ),
+                const SizedBox(height: 30),
+                // Botón de inicio de sesión
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción de inicio de sesión
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange, // Color del botón
+                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    textStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: const Text('Iniciar Sesión'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
